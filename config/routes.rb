2bcd521661e9f564cb0247976b6044fd7c0c2029
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-11 14:26:53 +0900

    Completed skeleton of lists controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,27 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # List all watchlists
+  # Show one watchlist
+  # Create a watchlist
+  resources :lists, only: [:index, :show, :new, :create]
+  # Add bookmark to list
+  # Delete bookmark from list
 end
+
+# LISTS:
+# A user can see all the lists
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
+
+# BOOKMARKS:
+# A user can add a new bookmark (movie/list pair) to an existing list
+# GET "lists/42/bookmarks/new"
+# POST "lists/42/bookmarks"
+# A user can delete a bookmark from a list. How can we make a delete link again?
+# DELETE "bookmarks/25"
